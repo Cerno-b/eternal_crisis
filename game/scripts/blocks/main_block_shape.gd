546731f45dfd6a53fe1de78aaa60377a3fa27b3e
Cerno_b <cerno.b@gmail.com>
2022-04-04 01:24:39 +0200

    Some more juice

--- game/scripts/blocks/main_block_shape.gd
+++ game/scripts/blocks/main_block_shape.gd
@@ -3,8 +3,9 @@ extends CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.position.x = -get_parent().in_node[0] + 22
-	self.position.y = -get_parent().in_node[1] + 22
+	pass
+	# self.position.x = -get_parent().in_node[0] + 22
+	#self.position.y = -get_parent().in_node[1] + 22
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
